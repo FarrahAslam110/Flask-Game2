30abda1b7ee189eda69a1d36294ff602c624a9dd
FarrahAslam110 <54948029+FarrahAslam110@users.noreply.github.com>
2023-09-27 20:43:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,30 +16,6 @@ def index():
 def print_name(name):
     return 'Hi ,{}'.format(name)
 
-@app.route('/synonyms/<string:fr>',methods=['GET','POST'])
-def synonyms(fr):
-    if request.method == 'GET':
-        if len(fr) > 0 :
-            syns1 = synonyms_get.create_synonym_dict()
-            
-            # Example usage
-            #original_sentence = "The doll was found in garage. The toy could be sold now. The doll was replaced with a dog. "
-            #original_sentence ="The software will be used by user to print account report. The report will be printable by employees through system."
-            #original_sentence = 'run race stroll rush nice lovely mean kind'
-            #original_sentence = "Only collision estimators shall search for recycled parts.  Users without the collision estimator role shall not have access to the search for recycled parts."
-            #original_sentence = "Movies can only be streamed if the customer has purchased a movie  and is within the 2-day time period to stream the movie.Daily usage statistics should be logged  and accessible by the user."
-            #original_sentence = "The product will require collaboration with a database management system (DBMS).The DBMS may be located on the same machine as the product or on a separate machine residing on the same computer network."
-            original_sentence = fr
-            new_sentence = synonyms_get.replace_synonyms_in_sentence(original_sentence,syns1)
-            #print("Original Sentence:", original_sentence)
-            new_sentence= sorted(new_sentence, key=lambda x: x[0])
-            #print("Modified Sentence:", new_sentence )
-
-            return jsonify(new_sentence)
-            #return jsonify(syns)
-        else:
-            return 'Nothing Found',404
-
 @app.route('/specify/<string:sent>',methods=['GET','POST'])
 def specify(sent):
     if request.method == 'GET':
@@ -66,73 +42,5 @@ def specify(sent):
         else:
             return 'Nothing Found',404
 
-'''
-def get_suggestions(paragraph,syns1):
-  #paragraph = input("Enter a paragraph: ")
 
-  corrected_paragraph = synonyms_get.correct_spelling(paragraph)
-  filtered_sentence = []
-  nouns = []
-  proper_nouns = []
-  verbs = []
-  #sentences = sent_tokenize(paragraph)
-  sentences = paragraph
-  sent=paragraph
-  #for sent in sentences:
-  if 1==1:
-      doc = nlp(sent)
-      for chunk in doc.noun_chunks:
-        print(f'******{chunk.text}*******')
-        #proper_nouns.append(remove_stop_words(chunk.text))
-      #stop words removal
-      filtered_sentence = synonyms_get.remove_stop_words(sent)
-      #words = word_tokenize(filtered_sentence)
-      words = synonyms_get.word_tokenize(sent)
 
-      pos_tags = nltk.pos_tag(words)
-
-      for word, pos in pos_tags:
-
-            if pos.startswith('NN'):  # Checking if the word is a noun
-              nouns.append(word)
-            if pos.startswith('NNP'): # If the word is a proper noun
-              proper_nouns.append(word)
-            if pos=='VB': # If the word is a proper noun
-              verbs.append(word)
-
-  print("\nOriginal Paragraph:")
-  print(paragraph)
-
-  print("\nCorrected Paragraph:")
-  print(corrected_paragraph)
-
-  print("\nStop Word Removed: ")
-  print(filtered_sentence)
-  
-  for noun in nouns:
-      synonyms1 = get_synonyms(noun)
-      synonyms1 = list(set(synonyms1))#unique synonyms
-
-      print(f"Noun:\n")
-      print(f"Synonyms for '{noun}': {', '.join(synonyms1)}")
-
-  for noun in proper_nouns:
-      synonyms = get_synonyms(noun)
-      print(f"Proper Noun:\n")
-      print(f"Synonyms for '{noun}': {', '.join(synonyms)}")
-
-  for verb in verbs:
-      verb=remove_stop_words(verb)
-      if verb:
-        synonyms2 = get_synonyms(verb)
-        synonyms2 = list(set(synonyms2))
-
-        print(f"Verbs\n ")
-        print(f"Synonyms for '{verb}': {', '.join(synonyms2)}")
-  
-
-  print(synonyms_get.replace_synonyms_in_sentence(corrected_paragraph,syns1))
-  return synonyms_get.replace_synonyms_in_sentence(corrected_paragraph,syns1)
-'''
-if __name__ == '__main__':
-    app.run()
